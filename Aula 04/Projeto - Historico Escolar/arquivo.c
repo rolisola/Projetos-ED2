0b8baa818effade42282b878c278e4e04db4c4ae
Vitor Rolisola <66443241+rolisola@users.noreply.github.com>
2024-08-29 19:05:37 -0300

    Alterações em 'arquivos.*' New: função para obter tamanho do arquivo Update: tratamento de erros

--- Aula 04/Projeto - Historico Escolar/arquivo.c
+++ Aula 04/Projeto - Historico Escolar/arquivo.c
@@ -3,7 +3,6 @@
 #include <string.h>
 #include "arquivo.h"
 
-
 // Funcao para verificar se o modo e valido
 int validar_modo(const char *modo){
     // Modos permitidos
@@ -16,51 +15,59 @@ int validar_modo(const char *modo){
             return 1; // Modo valido
         }
     }
+
     return 0; // Modo invalido
 }
 
 // Funcao para abrir um arquivo
 FILE *abrir_arquivo(const char *nome_arquivo, const char *modo){
     if(nome_arquivo == NULL || modo == NULL){
-        fprintf(stderr, "Nome do arquivo ou modo e NULL.\n");
+        printf("Nome do arquivo ou modo e NULL.\n");
         return NULL;
     }
 
     if(!validar_modo(modo)){
-        fprintf(stderr, "Modo invalido: (%s)\n", modo);
+        printf("Modo invalido: (%s)\n", modo);
         return NULL;
     }
 
     return fopen(nome_arquivo, modo);
 }
 
-// Funcao para criar um arquivo
+// Função para criar um arquivo
 FILE *criar_arquivo(const char *nome_arquivo, const char *modo){
     if(nome_arquivo == NULL || modo == NULL){
-        fprintf(stderr, "nome_arquivo or modo is NULL.\n");
+        fprintf(stderr, "Nome do arquivo ou modo é NULL.\n");
         return NULL;
     }
 
-    // Ajustar modo para criar o arquivo se necessario
+    // Ajustar modo para criar o arquivo se necessário
     if(strchr(modo, 'r') != NULL){
-        // Modificar o modo para 'w+' se o arquivo nao existir
-        return fopen(nome_arquivo, "w+");
+        // Verifica se o modo e binário
+        if(strchr(modo, 'b') != NULL){
+            // Se o modo e binário, usar "wb+"
+            return fopen(nome_arquivo, "wb+");
+        }else{
+            // Caso contrário, usar "w+"
+            return fopen(nome_arquivo, "w+");
+        }
     }else{
         fprintf(stderr, "Nao pode criar arquivo com o modo: (%s)\n", modo);
         return NULL;
     }
 }
 
-// Funcao para abrir ou criar um arquivo
+// Função para abrir ou criar um arquivo
 FILE *abrir_criar_arquivo(const char *nome_arquivo, const char *modo){
     FILE *arquivo = abrir_arquivo(nome_arquivo, modo);
 
     if(arquivo == NULL){
         if(errno == ENOENT){
-            // Se o arquivo nao existe, cria o arquivo
+            // Se o arquivo não existe, cria o arquivo
+            printf("Arquivo inexistente. Criando.\n");
             arquivo = criar_arquivo(nome_arquivo, modo);
         }else{
-            // Se o erro nao for 'file not found', exibe mensagem de erro
+            // Se o erro não for 'file not found', exibe mensagem de erro
             perror("Erro ao abrir arquivo");
         }
     }
@@ -68,17 +75,25 @@ FILE *abrir_criar_arquivo(const char *nome_arquivo, const char *modo){
     return arquivo;
 }
 
-// Funcao para fechar um arquivo
+// Função para fechar um arquivo
 int fechar_arquivo(FILE *arquivo){
     if(arquivo == NULL){
         fprintf(stderr, "Ponteiro do arquivo invlido.\n");
-        return 1; // Codigo de erro
+        return 1; // Código de erro
     }
 
     if(fclose(arquivo) != 0){
         perror("Erro ao fechar arquivo.\n");
-        return 1; // Codigo de erro
+        return 1; // Código de erro
     }
 
-    return 0; // Codigo de sucesso
+    return 0; // Código de sucesso
+}
+
+size_t obter_tamanho_arquivo(FILE *arquivo){
+    fseek(arquivo, 0, SEEK_END); // Move o ponteiro para o final do arquivo
+    size_t tamanho = ftell(arquivo); // Pega a posição atual do ponteiro (tamanho do arquivo)
+    fseek(arquivo, 0, SEEK_SET); // Volta o ponteiro para o início do arquivo
+
+    return tamanho;
 }
--- Aula 04/Projeto - Historico Escolar/arquivo.h
+++ Aula 04/Projeto - Historico Escolar/arquivo.h
@@ -6,5 +6,6 @@ FILE *abrir_arquivo(const char *nome_arquivo, const char *modo);
 FILE *criar_arquivo(const char *nome_arquivo, const char *modo);
 FILE *abrir_criar_arquivo(const char *nome_arquivo, const char *modo);
 int fechar_arquivo(FILE *arquivo);
+size_t obter_tamanho_arquivo(FILE *arquivo);
 
 #endif // ARQUIVO_H_INCLUDED
